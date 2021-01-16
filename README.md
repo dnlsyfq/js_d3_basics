8bee8205d68c51e84a4b2a51e1d48d34f7a4e226
= <dan90@live.com.my>
2021-01-16 20:42:30 +0800

    up8

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ npm install -g live-server
 
 live-server
 
+https://webplatform.github.io/docs/svg/
+
+map data to visual = data binding
---
+++ data-binding/app.js
@@ -0,0 +1,4 @@
+// d3.select("div").append("p").text("Hello World").style("color","green")
+
+d3.selectAll("div").append("p").text("Hello World").style("color","green")
+
