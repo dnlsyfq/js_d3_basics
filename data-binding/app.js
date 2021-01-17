2f309b1f480d1036cfbdbf837e531612ebf62212
= <dan90@live.com.my>
2021-01-17 15:23:30 +0800

    up8

--- data-binding/app.js
+++ data-binding/app.js
@@ -1,4 +1,14 @@
 // d3.select("div").append("p").text("Hello World").style("color","green")
 
-d3.selectAll("div").append("p").text("Hello World").style("color","green")
+/*
+d3.selectAll("div")
+    .append("p")
+    .text("Hello World")
+    .style("color","green")
 
+ */
+
+
+const dataset = [2,4,6,8,10]
+
+d3.select("#viz").selectAll('p').data(dataset).join('p').text((d,i) => `ID:${i} = ${d}`);
